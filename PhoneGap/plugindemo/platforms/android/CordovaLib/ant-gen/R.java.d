556c35878287a23bc8f81d8d27eae64b89f2dcfd
Heather Spector <HeatherNSpector@Heathers-MacBook-Pro.local>
2014-07-15 14:56:32 -0400

    Remove PhoneGap

--- PhoneGap/plugindemo/platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/HeatherNSpector/Documents/Cosi152aj/PhoneGap/plugindemo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
--- PhoneGap/plugindemo/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/HeatherNSpector/Documents/Cosi152aj/PhoneGap/plugindemo/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/HeatherNSpector/Documents/Cosi152aj/PhoneGap/plugindemo/platforms/android/AndroidManifest.xml \
