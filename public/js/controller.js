6d82f28761f1a77930c27165781492fe9e3282b7
Heather Spector <HeatherNSpector@Heathers-MacBook-Pro.local>
2014-07-31 14:49:47 -0400

    working nicer login UI

--- public/js/controller.js
+++ public/js/controller.js
@@ -52,10 +52,8 @@ var bimprovedApp = (function($) {
 	 function advancedSearch() {
     	if (document.getElementById('advSearchCB').checked) {
     		document.getElementById('advSearch').style.display = 'table';
-    		document.getElementById('searchFilter').style.display = 'none';
     	} else {
     		document.getElementById('advSearch').style.display = 'none';
-    		document.getElementById('searchFilter').style.display = 'table';
     	}
     }
 	 function valiDate() {
@@ -295,8 +293,10 @@ var bimprovedApp = (function($) {
     }
 	function loggedIn(){
         if(myList.user!="none"){
+        document.getElementById('cssmenu').style.display = 'block';
 		return true;
 		}
+		document.getElementById('cssmenu').style.display = 'none';
 		return false;
     }
     
