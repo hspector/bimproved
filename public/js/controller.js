64179ad09203a3e9df9176bfe235c0965809d30a
Heather Spector <HeatherNSpector@Heathers-MacBook-Pro.local>
2014-07-31 13:03:27 -0400

    Reverting to fixed login problem

--- public/js/controller.js
+++ public/js/controller.js
@@ -52,15 +52,10 @@ var bimprovedApp = (function($) {
 	 function advancedSearch() {
     	if (document.getElementById('advSearchCB').checked) {
     		document.getElementById('advSearch').style.display = 'table';
+    		document.getElementById('searchFilter').style.display = 'none';
     	} else {
     		document.getElementById('advSearch').style.display = 'none';
-    	}
-    }
-    function hideMenu() {
-    	if (bimprovedApp.loggedIn() == true) {
-    		document.getElementById('advSearch').style.display = 'inline';
-    	} else {
-    		document.getElementById('advSearch').style.display = 'none';
+    		document.getElementById('searchFilter').style.display = 'table';
     	}
     }
 	 function valiDate() {
